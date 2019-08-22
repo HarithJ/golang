cd88d8b9956e0654b0b1340501383f84b336c8e0
harithj <harithjaved@gmail.com>
2019-08-22 16:07:15 +0300

    - create a pod from a file

--- app/src/create-pod/main.go
+++ app/src/create-pod/main.go
@@ -1,24 +1,21 @@
 package main
 
 import (
+	"io/ioutil"
+	"log"
+
 	"create-pod/kubernetes/auth"
 	"create-pod/kubernetes/pod"
 )
 
-var json = `
-apiVersion: v1
-kind: Pod
-metadata:
-  name: pod-created
-spec:
-  containers:
-    - name: nginx
-      image: nginx:1.7.9
-      ports:
-      - containerPort: 80
-`
-
 func main() {
 	clientset := auth.GetClientSet()
-	pod.CreatePod(clientset, json)
+
+	podConfig, err := ioutil.ReadFile("pod.yml")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	pod.CreatePod(clientset, string(podConfig))
 }
